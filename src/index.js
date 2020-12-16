a0243745e3ae25871ec0256dbc848ddb0a84f6e3
Paweł Gruba <pgruba@interia.pl>
2020-12-16 19:50:43 +0100

    Components and props final

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import App from "./components/App";
 
 // const myName = "Pawel";
 // const element = <h1>Hello {myName} element</h1>;
+const componentElement = <App />; //always start component names with capital letter
 // ReactDOM.render(element, document.getElementById("app"));
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(componentElement, document.getElementById("app"));
 
 function tick() {
   const element = (
