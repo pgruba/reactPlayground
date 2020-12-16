a3839c2157c58ff586cf34b8013681b0cd092287
Paweł Gruba <pgruba@interia.pl>
2020-12-16 19:45:00 +0100

    Component and props part 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
 import React from "react";
 
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+const user = {
+  firstName: "Pawel",
+  lastName: "Gruba",
+};
+const secondUser = { age: 40 };
+
+// const element = (
+//   <div>
+//     <h1>
+//       Learning React by {formatName(user)} from element. Btw, secondUser age is {this.props.secondUser.age}
+//     </h1>
+//   </div>
+// );
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Learning React</h1>
+        <WelcomeHeader secondUser={secondUser} />
+        <Welcome name="Tadzio" />
+      </div>
+    );
+  }
+}
+
+class WelcomeHeader extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1>
+          Learning React by {formatName(user)} from element. Btw, secondUser age is {this.props.secondUser.age}
+        </h1>
       </div>
     );
   }
 }
 
 export default App;
+
+//function  component practice:
+function Welcome(props) {
+  return <h3>Hello my dear {props.name}</h3>;
+}
--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+// const myName = "Pawel";
+// const element = <h1>Hello {myName} element</h1>;
+// ReactDOM.render(element, document.getElementById("app"));
 ReactDOM.render(<App />, document.getElementById("app"));
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Ticker</h1>
+      <h2>{new Date().toTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("ticker"));
+}
+
+setInterval(tick, 1000);
